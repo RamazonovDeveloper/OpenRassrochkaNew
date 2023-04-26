4f0f8f34c8983753796ea7755c094423c965f35f
RamazonovDeveloper <ramazonovferuz01@gmail.com>
2023-04-26 23:09:32 +0500

    Footer added

--- src/App.js
+++ src/App.js
@@ -49,7 +49,11 @@ function App() {
         <div className='flex text-main_color font-medium text-[14px] mb-10'>
           <p className='uppercase'>{langData.service_name}</p>
         </div>
-        <p className='uppercase text-[25px] text-main_color leading-7 font-light'>Open <span className='text-black'>muddatli kartasi</span> - Bu biznesingiz o’sishi va rivojlanishi</p>
+        {/* <p className='uppercase text-[25px] text-main_color leading-7 font-light'>Open <span className='text-black'>muddatli kartasi</span> - Bu biznesingiz o’sishi va rivojlanishi</p> */}
+        {
+          lang === "uz" ? <p className='uppercase text-[25px] text-main_color leading-7 font-light'>Open <span className='text-black'>muddatli kartasi</span> - Bu biznesingiz o’sishi va rivojlanishi</p>
+          : <p className='uppercase text-[25px] text-main_color leading-7 font-light'><span className='text-black'>Карта рассрочки</span> open <span className='text-black'>- это</span> рост <span className='text-black'>и</span> развитие <span className='text-black'>вашего</span> бизнеса</p>
+        }
         <div className='text-base mt-4 font-medium'>
           <p>{langData.service_text1}</p>
           <p>{langData.service_text1}</p>
@@ -86,13 +90,34 @@ function App() {
 
       <div className='form_section relative h-[160px]'>
         <div className="container">
-          <div className="form_section_card py-[40px] px-[10px] bg-gray_form absolute top-3/4">
-            <h3 className='font-medium text-base uppercase'>{langData.partners_header}</h3>
-            <p className='text-[14px] my-6 font-medium'>{langData.partners_text}</p>
-            <input className='w-full py-3 px-2 bg-gray_input text-base text-black' type="text" placeholder={langData.partners_input_name}/>
-            <input className='w-full py-3 px-2 bg-gray_input text-base text-black' type="text" placeholder={langData.partners_input_tel}/>
-            {/* <input type="text" placeholder={langData.partners_input_tel}/> */}
-            <button className='w-full bg-main_color text-white py-3 uppercase mt-6 mb-10'>{langData.partners_btn}</button>
+          <div className="form_section_card xs:py-[40px] xs:px-[10px] sm:py-12 sm:px-4 bg-gray_form absolute top-3/4 md:flex">
+            <div className='md:w-1/2'>
+              <h3 className='font-medium text-base uppercase sm:text-[18px] lg:text-[20px]'>{langData.partners_header}</h3>
+              <p className='xs:text-[14px] sm:text-base my-6 font-medium lg:w-3/4 sm:mt-8'>{langData.partners_text}</p>
+            </div>
+            <div className='md:w-1/2'>
+              <input className='w-full py-3 px-2 bg-gray_input text-base text-black my_input' type="text" placeholder={langData.partners_input_name}/>
+              <input className='w-full py-3 px-2 bg-gray_input text-base text-black my_input xs:mt-4 xs:mb-6 sm:mt-5 sm:mb-11 xl:mb-[60px]' type="text" placeholder={langData.partners_input_tel}/>
+              {/* <input type="text" placeholder={langData.partners_input_tel}/> */}
+              {/*  xs:mt-6 xs:mb-10 sm:m */}
+              <button className='w-full bg-main_color text-white py-3 uppercase'>{langData.partners_btn}</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer mt-[500px] py-[78px]">
+        <div className="container">
+          <div className='bg-gray_light xs:py-8 xs:px-2.5 sm:p-[30px] max-w-sm mx-auto'>
+            <p className='uppercase font-medium'>{langData.decision}</p>
+            <div className='flex items-center mt-7 mb-[20px] font-medium'>
+              <img className='mr-4' src={message} alt="" />
+              <p>hello@opentech.uz</p>
+            </div>
+            <div className='flex items-center font-medium'>
+              <img className='mr-4' src={phone} alt="" />
+              <p> +998 (90) 968-7881</p>
+            </div>
           </div>
         </div>
       </div>
